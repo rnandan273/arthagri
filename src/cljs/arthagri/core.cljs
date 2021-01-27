7643c639c1bffd6bbcafb53e6d4709e88348fcf0
rnandan273 <raghu.sk@gmail.com>
2021-01-27 19:01:13 +0530

    updated

--- src/cljs/arthagri/core.cljs
+++ src/cljs/arthagri/core.cljs
@@ -1,24 +1,37 @@
 (ns arthagri.core
   (:require
-   [reagent.dom :as rdom]
+   [reagent.core :as reagent]
    [re-frame.core :as re-frame]
    [arthagri.events :as events]
    [arthagri.views :as views]
    [arthagri.config :as config]
+   ["aws-amplify" :default Amplify :as amp]
+   ["aws-amplify-react" :refer (withAuthenticator)]
+   
    ))
-
 
 (defn dev-setup []
   (when config/debug?
     (println "dev mode")))
 
+(def root-view
+  (reagent/adapt-react-class
+   (withAuthenticator
+    (reagent/reactify-component views/main-panel) true)))
+
 (defn ^:dev/after-load mount-root []
   (re-frame/clear-subscription-cache!)
-  (let [root-el (.getElementById js/document "app")]
-    (rdom/unmount-component-at-node root-el)
-    (rdom/render [views/main-panel] root-el)))
+  (.configure Amplify (clj->js {
+        :aws_project_region "us-east-2"
+        :aws_cognito_identity_pool_id "us-east-2:086cecc3-0f58-468f-a380-be64e7d6f6da"
+        :aws_cognito_region "us-east-2"
+        :aws_user_pools_id "us-east-2_ShI50MSgu"
+        :aws_user_pools_web_client_id "6jldtesda3gag5baosc1ctchkl",
+        :oauth {}}))
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (reagent.dom/render [root-view]
+                  (.getElementById js/document "app")))
 
 (defn init []
-  (re-frame/dispatch-sync [::events/initialize-db])
   (dev-setup)
   (mount-root))
